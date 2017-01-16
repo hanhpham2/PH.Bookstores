d8f8ce0a93ddba933523fded0b5a3b4a7930545b
hanh.pham2 <hanh.pham2@niteco.se>
2017-01-16 18:01:08 +0700

    reconfig

---
+++ 2.Data/PH.BookStores.Model/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace PH.BookStores.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("name=ApplicationDbContext")
+        {
+
+        }
+    }
+}
